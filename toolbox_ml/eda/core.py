7d325b8ca139158afd553892b634b38fe286336a
Gabriel Caruso <128610501+Gabriel-Caruso@users.noreply.github.com>
2026-06-05 14:30:11 +0200

    feat: añadida la función de análisis describe_df al completo y testeada

--- toolbox_ml/eda/core.py
+++ toolbox_ml/eda/core.py
@@ -12,16 +12,11 @@ def describe_df(df: pd.DataFrame) -> pd.DataFrame:
         print(f"El argumento es de tipo {type(df)} cuando debería ser un DataFrame")
         return None
     
-    tipo = df.dtypes
-    porcentaje_nulos = round((df.isnull().sum() / len(df)) * 100, 2)
-    valores_unicos = df.nunique()
-    porcentaje_cardinalidad = round((valores_unicos / len(df)) * 100, 2)
-
     columnas = pd.DataFrame({
-        "tipo" : tipo,
-        "porcentaje_nulos": porcentaje_nulos,
-        "valores_unicos": valores_unicos,
-        "porcentaje_cardinalidad": porcentaje_cardinalidad
+        "tipo" : df.dtypes,
+        "porcentaje_nulos": round((df.isnull().sum() / len(df)) * 100, 2),
+        "valores_unicos": df.nunique(),
+        "porcentaje_cardinalidad": round((df.nunique() / len(df)) * 100, 2)
     })
 
     return columnas
