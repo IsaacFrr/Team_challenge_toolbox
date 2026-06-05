4bf433de2793ffcc93c5302b4c32dbbfd36cffdb
Gabriel Caruso <128610501+Gabriel-Caruso@users.noreply.github.com>
2026-06-05 12:51:16 +0200

    feat: funcion describe_df + setup.py

---
+++ toolbox_ml/eda/core.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+
+
+# --- FUNCIONES DE ANÁLISIS ---
+def describe_df(df: pd.DataFrame) -> pd.DataFrame:
+    """Recibe un DataFrame y devuelve otro DataFrame con una fila 
+    por cada columna del DataFrame original. El índice del resultado debe ser 
+    el nombre de cada columna. Las columnas del resultado deben ser"""
+
+    if not isinstance(df, pd.DataFrame):
+        print(f"El argumento es de tipo {type(df)} cuando debe ser un DataFrame")
+        return None
+    
+    tipo = df.dtypes
+    porcentaje_nulos = (df.isnull().sum() / len(df)) * 100
+    valores_unicos = df.nunique()
+    porcentaje_cardinalidad = (valores_unicos / len(df)) * 100
+
+    columnas = pd.DataFrame({
+        "tipo" : tipo,
+        "porcentaje_nulos": porcentaje_nulos,
+        "valores_unicos": valores_unicos,
+        "porcentaje_cardinalidad": porcentaje_cardinalidad
+    })
+
+    return columnas
